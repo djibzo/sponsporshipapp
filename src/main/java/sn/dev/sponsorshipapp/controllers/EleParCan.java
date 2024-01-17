52a0879eb503243d3ef05194ca1b4124590adef7
djibzo <djibzo90@gmail.com>
2024-01-17 02:54:51 +0000

    finished

--- src/main/java/sn/dev/sponsorshipapp/controllers/EleParCan.java
+++ src/main/java/sn/dev/sponsorshipapp/controllers/EleParCan.java
@@ -14,6 +14,7 @@ import sn.dev.sponsorshipapp.entities.UserModel;
 import sn.dev.sponsorshipapp.entities.Utilisateur;
 import sn.dev.sponsorshipapp.tools.Notification;
 
+import javax.xml.transform.Source;
 import java.net.URL;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -43,6 +44,7 @@ public class EleParCan implements Initializable {
     private DBConnexion db = new DBConnexion();
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        //System.out.println(getNb());
         loadTable();
         idUserConn = UserModel.getUserId();
         if (aParrainer()) {
@@ -52,7 +54,10 @@ public class EleParCan implements Initializable {
     }
     public ObservableList<Utilisateur> getUsers() {
         ObservableList<Utilisateur> users = FXCollections.observableArrayList();
-        String sql = "SELECT * FROM user WHERE profil=2 ORDER BY nom ASC";
+        String sql = "SELECT * FROM user u " +
+                "LEFT JOIN (SELECT candidat, COUNT(*) AS nombreParrains FROM parrainer GROUP BY candidat) p ON u.id = p.candidat" +
+                " WHERE u.profil = 2 AND (p.nombreParrains IS NULL OR p.nombreParrains < 7) " +
+                "ORDER BY u.nom ASC;";
         try {
             db.initPrepar(sql);
             ResultSet rs = db.executeSelect();
@@ -122,4 +127,38 @@ public class EleParCan implements Initializable {
         Notification.NotifSuccess("Parrainage validé !","Votre parrainage a été enregistré avec succes !");
         parrainerBtn.setDisable(true);
     }
+    public ObservableList<Utilisateur>getStats(){
+        ObservableList<Utilisateur> can =  FXCollections.observableArrayList();
+        String sql = "SELECT CONCAT(UPPER(prenom),' ',UPPER(nom)) AS CANDIDAT,COUNT(*) AS NOMBRE_PARRAINS FROM parrainer,user WHERE parrainer.candidat=user.id GROUP BY user.id;";
+        try {
+            db.initPrepar(sql);
+            ResultSet rs = db.executeSelect();
+            while (rs.next()) {
+                Utilisateur u=new Utilisateur(rs.getInt("NOMBRE_PARRAINS"),rs.getString("CANDIDAT"));
+                can.add(u);
+            }
+            db.closeConnection();
+        } catch (SQLException e) {
+            throw new RuntimeException();
+        }
+        return can;
+    }
+    ///
+//    public List<Integer> getNb() {//selectionne la liste des electeur
+//        List<Integer> pars = new ArrayList<>();
+//        String sql = " SELECT user.id AS ID,COUNT(*) AS NOMBRE_PARRAINS FROM parrainer,user WHERE parrainer.candidat=user.id GROUP BY user.id";
+//        try {
+//            db.initPrepar(sql);
+//            ResultSet rs = db.executeSelect();
+//            while (rs.next()) {
+//                Utilisateur u=new Utilisateur(rs.getInt("NOMBRE_PARRAINS"),rs.getString("CANDIDAT"));
+//                pars.add(rs.getInt("ID"));
+//                pars.add(rs.getInt("NOMBRE_PARRAINS"));
+//            }
+//            db.closeConnection();
+//        } catch (SQLException e) {
+//            throw new RuntimeException();
+//        }
+//        return pars;
+//    }
 }
